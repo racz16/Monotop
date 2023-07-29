cb83968d0b4e5f6759330b3f083b0cb49d7859c0
Rácz Zalán <zalan.racz.dev@gmail.com>
2023-07-30 01:19:26 +0200

    Creating the rectangles of dots

---
+++ src/app/shared/dots/dots.component.ts
@@ -0,0 +1,66 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Rectangle } from '../interfaces/rectangle';
+
+@Component({
+    selector: 'app-dots',
+    templateUrl: './dots.component.html',
+    styleUrls: ['./dots.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DotsComponent {
+    @Input() emptyColor = 'transparent';
+    @Input() normalColor = '#62e067';
+    @Input() intersectionColor = '#555f6733';
+    @Input() rectangles: Rectangle[] = [];
+
+    getColumnIndices(): number[] {
+        const rectangles = this.getValidRectangles();
+        const size = rectangles.length ? Math.max(...rectangles.map((rectangle) => rectangle.x + rectangle.width)) : 0;
+        return this.getIndices(size);
+    }
+
+    getRowIndices(): number[] {
+        const rectangles = this.getValidRectangles();
+        const size = rectangles.length ? Math.max(...rectangles.map((rectangle) => rectangle.y + rectangle.height)) : 0;
+        return this.getIndices(size);
+    }
+
+    private getValidRectangles(): Rectangle[] {
+        return (
+            this.rectangles?.filter(
+                (rectangle) => rectangle.x >= 0 && rectangle.y >= 0 && rectangle.width > 0 && rectangle.height > 0
+            ) ?? []
+        );
+    }
+
+    private getIndices(size: number): number[] {
+        return new Array(size).fill(0).map((_, i) => {
+            return i;
+        });
+    }
+
+    getColor(x: number, y: number): string {
+        let filled = 0;
+        for (const rectangle of this.rectangles) {
+            if (this.isInsideTheRectangle(x, y, rectangle)) {
+                filled++;
+            }
+        }
+        if (filled === 0) {
+            return this.emptyColor;
+        } else if (filled === 1) {
+            return this.normalColor;
+        } else {
+            return this.intersectionColor;
+        }
+    }
+
+    private isInsideTheRectangle(x: number, y: number, rectangle: Rectangle): boolean {
+        return (
+            rectangle.x <= x &&
+            x < rectangle.x + rectangle.width &&
+            rectangle.y <= y &&
+            y < rectangle.y + rectangle.height
+        );
+    }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DotsComponent } from './dots/dots.component';
 import { LinkButtonComponent } from './link-button/link-button.component';
 
 @NgModule({
-    declarations: [LinkButtonComponent],
+    declarations: [LinkButtonComponent, DotsComponent],
     imports: [CommonModule, RouterModule],
-    exports: [LinkButtonComponent],
+    exports: [LinkButtonComponent, DotsComponent],
 })
 export class SharedModule {}
