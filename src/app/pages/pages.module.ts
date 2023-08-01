a57c1df62efabc04c2b5e1cdc1f993d256ddbc25
Rácz Zalán <zalan.racz.dev@gmail.com>
2023-08-01 18:57:06 +0200

    Creating the contact component and adding it to the routing

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
-    declarations: [MainPageComponent],
+    declarations: [MainPageComponent, ContactPageComponent],
     imports: [CommonModule, SharedModule],
 })
 export class PagesModule {}
